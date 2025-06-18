6e49710fd14f6a5bea40380c3d350879102a388c
lorhainy <lorhainy.silva@escola.pr.gov.br>
2025-06-18 09:18:39 -0300

    18

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'o que é alura?'
+    ' Alura é a maior escola online de tecnologia do Brasil, que oferece cursos nas áreas de Programação, Front-End, DevOps, Mobile, Data Science, UX & Design, Inteligência Artificial e Gestão & Inovação.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'onde foi a primeira guerra?',
+    'A primeira guerra ocorreu na Europa'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é o GitHub?',
+    'é uma plataforma online que funciona como um serviço de hospedagem para repositórios Git'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz gato em ingles?',
+    'gato se diz CAT'
 )
